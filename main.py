56cd851f1f9d7b10105ed4da91c71b98b2d18d60
Isabella Graziolo <isabellagraziolo@gmail.com>
2019-12-11 20:59:05 -0500

    Pretty Thang

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import sys
 from PIL import Image
 
@@ -9,24 +10,27 @@ output_path = sys.argv[2]
 img = Image.open(input_path)
 width, height = img.size
 
-# Create a new, all-white image that's the same size as the original
 new_img = Image.new("RGB", (width, height), "white")
 
-# TODO: Replace this with your own filter!
-# Median pixel filter, taken from https://note.nkmk.me/en/python-opencv-pillow-image-size
-members = [(0, 0)] * 9
-for i in range(1, width - 1):
+for i in range(1, width // 12):
     for j in range(1, height - 1):
-        members[0] = img.getpixel((i - 1, j - 1))
-        members[1] = img.getpixel((i - 1, j))
-        members[2] = img.getpixel((i - 1, j + 1))
-        members[3] = img.getpixel((i, j - 1))
-        members[4] = img.getpixel((i, j))
-        members[5] = img.getpixel((i, j + 1))
-        members[6] = img.getpixel((i + 1, j - 1))
-        members[7] = img.getpixel((i + 1, j))
-        members[8] = img.getpixel((i + 1, j + 1))
-        members.sort()
-        new_img.putpixel((i, j), (members[4]))
+        r, g, b = img.getpixel((i, j))
+        new_img.putpixel((i, j), (r, 250, 250))
+for i in range(1, width // 12):
+    for j in range(1, height // 12):
+        r, g, b = img.getpixel((i, j))
+        new_img.putpixel((i, j), (r, g, b))
+for i in range(1, width // 12):
+    for j in range(height // 12, height // 6):
+        r, g, b = img.getpixel((i, j))
+        new_img.putpixel((i, j), (255, 255, int(b/2)))
+for i in range(width // 12, (2 * width) // 3):
+    for j in range(1, height - 1):
+        r, g, b = img.getpixel((i, j))
+        new_img.putpixel((i, j), (255, g, 100))
+for i in range((2 * width) // 3, width):
+    for j in range(1, height - 1):
+        r, g, b = img.getpixel((i, j))
+        new_img.putpixel((i, j), (240, 170, b))
 
-new_img.save(output_path, "JPEG")
+new_img.save(output_path)
